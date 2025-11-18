642d22d56febd63c2cd22d591ebab1ac6e207d3c
=yasminehz <=173774288+yasminehz@users.noreply.github.com>
2025-11-18 14:30:58 +0100

    commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -94,7 +94,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
+ 
         return $this;
     }
 
